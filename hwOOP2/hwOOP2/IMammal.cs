50a0c3e6ac12006446932f8082b7fbeb32011e34
OanaBlanaru <oana_miha_b@yahoo.com>
2019-01-11 13:50:50 +0200

    rabbit

---
+++ hwOOP2/hwOOP2/IMammal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hwOOP2
+{
+    interface IMammal
+    {
+        void Moves();
+
+        void Sleeps();
+
+        void Eats(string food);
+    }
+}
--- hwOOP2/hwOOP2/Rabbit.cs
+++ hwOOP2/hwOOP2/Rabbit.cs
@@ -2,11 +2,54 @@
 
 namespace hwOOP2
 {
-    class Program
+    class Rabbit : IMammal
     {
+        enum EyeColor { blue, red, black }
+
+        enum FurColor { white, brown, black, grey }
+
+        enum Gender { male, female }
+
+        public DateTime BirthDate
+        {
+            get;
+
+            set;
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rabbit rabbit = new Rabbit();
+
+            Console.WriteLine($"Rabbit is {Gender.female}, has {FurColor.grey} fur color and {EyeColor.black} eye color");
+
+            Console.Write("Enter the birth date of the rabbit (as mm/dd/yyyy): ");
+            rabbit.BirthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Rabbit is {(DateTime.Now - rabbit.BirthDate).TotalDays / 365.2425} years old");
+
+            rabbit.Eats("carrots");
+            rabbit.Moves();
+            rabbit.Sleeps();
+
+        }
+
+        public void Moves()
+        {
+            Console.WriteLine("Rabbit moves fast");
+        }
+
+        public void Sleeps()
+        {
+            Console.WriteLine("Rabbits sleep at night");
+        }
+
+        public void Eats(string food)
+        {
+
+            Console.WriteLine($"Rabbit eats {food}");
         }
     }
+
 }
