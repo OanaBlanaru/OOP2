cb54e3a7a2d5dc22c7b9678014b0040696e0dd8c
OanaBlanaru <oana_miha_b@yahoo.com>
2019-01-11 13:43:32 +0200

    First commit

---
+++ hwOOP2/hwOOP2/Rabbit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hwOOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
